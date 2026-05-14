ef83cb936ca95313a8875e4ce0805ed045b04990
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-05-14 12:10:39 +0530

    Create 01.testbench.sv

---
+++ Verification_sv/02.D-FLIPFLOP/Synchronous reset/TESTBENCH/01.testbench.sv
@@ -0,0 +1,32 @@
+`include "transaction.sv"
+`include "interface.sv"
+`include "generator.sv"
+`include "driver.sv"
+`include "monitor.sv"
+`include "scoreboard.sv"
+`include "environment.sv"
+
+module testbench();
+  
+  reg clk=0;
+  always #5 clk=~clk;
+  
+  //connecting interface 
+  dff_itf itf(clk);
+  //connecting  dut
+  dff d1(.clk(clk),.rst(itf.rst),.d(itf.d),.q(itf.q));
+  
+  environment e;
+ 
+  initial begin
+    $dumpfile("v.vcd");
+    $dumpvars(0,testbench);
+    $display("===========================================================================");
+    $display("                      VERIFICATION DFF IN SV                               ");
+    $display("===========================================================================");
+    e=new(itf,itf);
+    e.run();
+    #1000 $finish;
+   end
+  
+endmodule
