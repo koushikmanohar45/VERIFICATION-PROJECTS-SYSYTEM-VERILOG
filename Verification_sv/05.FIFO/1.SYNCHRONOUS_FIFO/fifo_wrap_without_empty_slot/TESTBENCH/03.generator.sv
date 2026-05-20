29bbfca936531224c5152e75dd20593fef27c01f
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-05-20 19:52:55 +0530

    Create 03.generator.sv

---
+++ Verification_sv/05.FIFO/1.SYNCHRONOUS_FIFO/fifo_wrap_without_empty_slot/TESTBENCH/03.generator.sv
@@ -0,0 +1,22 @@
+class generator;
+  mailbox #(transaction) to_driver;
+  transaction t;
+  event done;
+  
+  function new(mailbox #(transaction) to_driver,event done);
+    this.to_driver=to_driver;
+    this.done=done;
+  endfunction
+  
+  task gen();
+    begin
+      repeat(20)begin
+        t=new();
+        t.randomize();
+        to_driver.put(t);
+        @(done);
+      end
+    end
+  endtask
+  
+endclass
