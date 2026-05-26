7c5645dc2de79fbce1460dd7d9d43e325041456a
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-05-26 09:14:26 +0530

    Create sync_fifo.sv

---
+++ Verification_sv/05.FIFO/1.SYNCHRONOUS_FIFO/FIFO_with_empty_slot/DESIGN/sync_fifo.sv
@@ -0,0 +1,40 @@
+module sync_fifo(
+  input[7:0] data_in,
+  input rd_en,wt_en,clk,rst,
+  output reg [7:0] data_out,
+  output full,empty
+);
+  reg[7:0]mem[63:0];
+  reg [5:0] wptr,rptr;
+
+  always@(posedge clk) begin
+    if(rst) begin
+      wptr<=6'd0;
+      data_out<=8'd0;
+    end
+   else begin
+     if(wt_en && !full) begin 
+           mem[wptr]<=data_in;
+           wptr<=wptr+1;
+       end
+     else
+         wptr<=wptr;
+      end
+   end
+
+ always @(posedge clk) begin
+  if(rst)
+     rptr<=6'd0;       
+  else begin 
+    if(rd_en && !empty) begin
+         data_out<=mem[rptr];
+         rptr<=rptr+1;
+        end
+       else
+         rptr<=rptr;
+      end
+   end
+
+  assign empty=(wptr==rptr);
+  assign full=(rptr==(wptr+1'b1));
+endmodule
