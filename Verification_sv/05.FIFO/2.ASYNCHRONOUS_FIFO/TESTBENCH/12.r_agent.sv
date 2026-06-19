2d7efd28b769ba109acf4c546cc371229864ad68
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-06-19 16:37:00 +0530

    Create 12.r_agent.sv

---
+++ Verification_sv/05.FIFO/2.ASYNCHRONOUS_FIFO/TESTBENCH/12.r_agent.sv
@@ -0,0 +1,42 @@
+class r_agent;
+  
+  mailbox #(r_transaction) r_gen_dri;
+  mailbox #(r_transaction) r_mon_scb;
+  
+   r_generator g;
+   r_driver d;
+   r_monitor m;
+  
+  event done;
+  
+  virtual fifo.r_dr vif;
+  virtual fifo.r_mtr vif1;
+  
+  function new(mailbox #(r_transaction) r_mon_scb,virtual fifo.r_dr vif,virtual fifo.r_mtr vif1);
+    
+    this.r_mon_scb=r_mon_scb;
+    this.vif=vif;
+    this.vif1=vif1;
+
+    r_gen_dri=new();
+    
+    g=new(r_gen_dri,done);
+    d=new(r_gen_dri,vif,done);
+    m=new(r_mon_scb,vif1);
+    
+  endfunction
+  
+  
+  task a2_run();
+    begin
+        fork
+          g.g_gn();
+          d.d_rn();
+          d.rst_synchronize();
+          m.m_rn();
+        join_none
+    end
+  endtask
+
+  
+endclass
