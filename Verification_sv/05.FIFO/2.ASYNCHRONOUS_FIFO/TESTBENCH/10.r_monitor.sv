5fb303e3b1b6ddacae857a99cb78072a9453080d
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-06-19 16:36:13 +0530

    Create 10.r_monitor.sv

---
+++ Verification_sv/05.FIFO/2.ASYNCHRONOUS_FIFO/TESTBENCH/10.r_monitor.sv
@@ -0,0 +1,30 @@
+class r_monitor;
+  
+  r_transaction t;
+  mailbox #(r_transaction) to_scoreboard;
+ 
+  virtual fifo.r_mtr vif;
+  
+  function new(mailbox #(r_transaction) to_scoreboard,virtual fifo.r_mtr vif);
+    this.to_scoreboard=to_scoreboard;
+    this.vif=vif;
+  endfunction
+  
+  task m_rn();
+    begin
+      forever begin
+        t=new();
+        
+        @(vif.r_cb_mon);
+        
+        t.r_en=vif.r_cb_mon.r_en;
+        t.r_rst=vif.r_rst;
+        t.data_out=vif.r_cb_mon.data_out;
+        t.empty=vif.r_cb_mon.empty;
+        to_scoreboard.put(t);
+        $display("[time=%0t] MONITOR[READ] : r_reset=%b | read_enable=%b  | data_out=%d |empty=%b ",$time, t.r_rst,t.r_en,t.data_out,t.empty);
+      end
+    end
+  endtask
+  
+endclass
