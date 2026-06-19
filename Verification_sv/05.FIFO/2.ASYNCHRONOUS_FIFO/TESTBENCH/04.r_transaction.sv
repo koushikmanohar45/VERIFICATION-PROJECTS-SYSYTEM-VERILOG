afbdc9d9e362165e02b6a2c7a7b1c94e12a9ce86
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-06-19 16:29:26 +0530

    Create 04.r_transaction.sv

---
+++ Verification_sv/05.FIFO/2.ASYNCHRONOUS_FIFO/TESTBENCH/04.r_transaction.sv
@@ -0,0 +1,25 @@
+class r_transaction #(parameter width=8);
+  
+
+  rand bit r_en;
+  bit r_rst;
+  bit[width-1:0] data_out;
+  bit empty;
+  int count;
+  
+  constraint c1{
+                if(count==0)
+                   r_en==0;//write only
+                if(count==1)
+                   r_en==0;//full
+                if(count==2)
+                   r_en==1;//read only
+                if(count==3)
+                   r_en==1;//empty
+                if(count==4)
+                   r_en==1;//simultanious read and write
+                if(count==5)
+                   r_en==0;//no read and write
+               }
+
+endclass
