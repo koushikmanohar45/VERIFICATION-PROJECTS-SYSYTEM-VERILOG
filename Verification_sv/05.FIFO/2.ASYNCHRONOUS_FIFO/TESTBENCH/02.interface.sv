dc721e25ec22d2cbba90c81da95a6d572cc271ad
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-07-17 14:31:46 +0530

    Update 02.interface.sv

--- Verification_sv/05.FIFO/2.ASYNCHRONOUS_FIFO/TESTBENCH/02.interface.sv
+++ Verification_sv/05.FIFO/2.ASYNCHRONOUS_FIFO/TESTBENCH/02.interface.sv
@@ -1,4 +1,4 @@
-interface fifo #(parameter depth=32,width=8 ) (input r_clk,input w_clk) ;  
+interface fifo #(parameter depth=16,width=8 ) (input r_clk,input w_clk) ;  
   
   logic r_rst;
   logic w_rst; 
@@ -8,29 +8,31 @@ interface fifo #(parameter depth=32,width=8 ) (input r_clk,input w_clk) ;
   logic [width-1:0] data_out;
   logic full;
   logic empty;
+  logic [$clog2(depth):0] w_ptr;
+  logic [$clog2(depth):0] r_ptr;
   
   clocking r_cb_drv @(negedge r_clk);
     default input #1 output #0;
-    input data_out,empty;
+    input data_out,empty,r_ptr;
     output r_en;
   endclocking
   
   clocking w_cb_drv @(negedge w_clk );
     default input #1 output #0;
-    input full;
+    input full,w_ptr;
     output data_in,w_en;
   endclocking
   
   clocking w_cb_mon @(posedge w_clk);
     default input #1 output #0;
-    input data_in,w_en;
-    input  full;
+    input data_in,w_en,w_ptr;
+    input #0 full;
   endclocking
   
   clocking r_cb_mon @(posedge r_clk);
     default input #1 output #0;
-    input data_out,r_en;
-    input  empty;
+    input data_out,r_en,r_ptr;
+    input #1 empty;
   endclocking
   
   modport w_mtr(input w_clk,w_rst,clocking w_cb_mon);
