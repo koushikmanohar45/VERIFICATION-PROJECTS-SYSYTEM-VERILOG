ba7f56973f927e359c31e62ddd99d7049f2645ff
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-07-17 14:37:44 +0530

    Update 09.w_monitor.sv

--- Verification_sv/05.FIFO/2.ASYNCHRONOUS_FIFO/TESTBENCH/09.w_monitor.sv
+++ Verification_sv/05.FIFO/2.ASYNCHRONOUS_FIFO/TESTBENCH/09.w_monitor.sv
@@ -2,11 +2,13 @@ class w_monitor;
   
   w_transaction t;
   mailbox #(w_transaction) to_scoreboard;
+  mailbox #(w_transaction) to_coverage;
  
   virtual fifo.w_mtr vif;
   
-  function new(mailbox #(w_transaction) to_scoreboard,virtual fifo.w_mtr vif);
+  function new(mailbox #(w_transaction) to_scoreboard,mailbox #(w_transaction) to_coverage,virtual fifo.w_mtr vif);
     this.to_scoreboard=to_scoreboard;
+    this.to_coverage=to_coverage;
     this.vif=vif;
   endfunction
   
@@ -20,7 +22,9 @@ class w_monitor;
         t.data_in=vif.w_cb_mon.data_in;
         t.w_rst=vif.w_rst;
         t.w_en=vif.w_cb_mon.w_en;
+        t.w_ptr=vif.w_cb_mon.w_ptr;
         to_scoreboard.put(t);
+        to_coverage.put(t);
         $display("[time=%0t] MONITOR[WRITE]: w_reset=%b | write_enable=%b | data_in=%d |full=%b ",$time, t.w_rst,t.w_en,t.data_in,t.full);
       end
     end
